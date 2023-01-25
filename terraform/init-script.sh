44fedc12613b722bcb0828cbb5ee334f533bfdd8
Belendor <dwarf11@gmail.com>
2023-01-25 12:36:19 +0200

    update

--- terraform/init-script.sh
+++ terraform/init-script.sh
@@ -8,5 +8,26 @@ sudo sed -i 's/debian/ubuntu/g' /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-# Start Jenkins
-docker run -d  -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock belendor/jenkins
+#Java
+sudo apt update
+sudo apt install openjdk-11-jre -y
+
+#Jenkins
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+
+# curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+#   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+# echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+#   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+#   /etc/apt/sources.list.d/jenkins.list > /dev/null
+# sudo apt-get update
+# sudo apt-get install jenkins
+# docker run -d  -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock belendor/jenkins
+# chmod 777 /var/run/docker.sock
