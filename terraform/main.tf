c1215aa177c3745b35d1e2bbea78152286fc2479
Belendor <dwarf11@gmail.com>
2023-01-25 21:46:28 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,6 +64,13 @@ resource "aws_instance" "jenkins" {
   key_name               = aws_key_pair.generated_key.key_name
   subnet_id              = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
   vpc_security_group_ids = [aws_security_group.my-sg.id]
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 32
+    volume_type = "gp2"
+  }
+
   user_data = templatefile("${path.module}/init-script.sh", {
     file_content = "temp content"
   })
